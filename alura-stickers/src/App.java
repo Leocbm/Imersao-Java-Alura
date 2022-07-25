1ac70fdbd075762b53191ee622b8f6725707537c
Leonardo Cunha <leonardoleo.barbosa2@gmail.com>
2022-07-25 15:55:44 -0300

    Adicionando novo aprendizado da aula 02 do curso

--- alura-stickers/src/App.java
+++ alura-stickers/src/App.java
@@ -1,4 +1,6 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -7,9 +9,12 @@ import java.util.List;
 import java.util.Map;
 
 public class App {
+
     public static void main(String[] args) throws Exception {
 
         // fazer uma conexão HTTP e buscar os top 250 filmes
+
+        //String url = "https://imdb-api.com/en/API/Top250Movies/k_0ojt0yvm";
         String url = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
         URI endereco = URI.create(url);
         var client = HttpClient.newHttpClient();
@@ -17,16 +22,28 @@ public class App {
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
 
-        // extrair só os dados que interessam (título, poster, classificação)
+        // extrair só os dados que interessam (titulo, poster, classificação)
+
         var parser = new JsonParser();
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
-        System.out.println(listaDeFilmes.size());
 
-        // exibir e manipular dados
+        // exibir e manipular os dados 
+
+        var geradora = new GeradoraDeFigurinhas();
         for (Map<String,String> filme : listaDeFilmes) {
-            System.out.println(filme.get("title"));
-            System.out.println(filme.get("image"));
-            System.out.println(filme.get("imDbRating"));
+
+            String urlImagem = filme.get("image");
+            String titulo = filme.get("title");
+            
+            try{
+                InputStream inputStream = new URL(urlImagem).openStream();
+                String nomeArquivo = titulo.replace(":", "-") + ".png";
+                System.out.println("Gerando imagem - [" + nomeArquivo + "]");
+                geradora.cria(inputStream, nomeArquivo);
+             }catch(java.io.FileNotFoundException err){
+                  System.out.println("Imagem não encontrada ou link inválido");
+             }
+
             System.out.println();
         }
     }
